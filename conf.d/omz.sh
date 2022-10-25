580d08489985e22c4ba84d326a0dcd55ffd3ddf1
Logank <logan.aker1989@gmail.com>
2022-10-24 23:08:34 -0400

    Optimizing code for universality.

---
+++ conf.d/omz.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Installs oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
--- portunus.sh
+++ portunus.sh
@@ -1,41 +1,62 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# A script to set up your linux machine the way logank would want it set up
-export portunus=/home/$USER/portunus
+# Download and install commonly used and particularly useful applications
 
-function prep {
+export PORTUNUS=/home/$USER/portunus
+
+#########################################
+# Update and upgrade apt package manager
+# and verify wget is currently installed.
+# Arguments:
+#   None
+# Outputs:
+#   Writes status to stdout
+#########################################
+prep() {
     echo "Setting things up. . ."
-    # Update apt sources
     sudo apt update && sudo apt upgrade
-# Ensure wget is installed before beginning install process
-    if ! [ -f "/usr/bin/wget" ] ; then
+    if ! [ -f "/usr/bin/wget" ]; then
         sudo apt install wget -y
-        fi
+    fi
     echo "Prep complete."
 }
 
-# Install basic applications and dependencies via apt
-function deliver {
+#########################################
+# Parse the config file and install
+# relevant applications.
+# Arguments:
+#   None
+# Outputs:
+#   Writes status to stdout
+#########################################
+deliver() {
     echo "Installing applications..."
     source ./conf.d/packages.cfg
     sudo apt install $packages -y
     sudo bash ./conf.d/gh.sh
     sudo bash ./conf.d/vscode.sh
     echo "Installation complete"
-    clear
 }
 
-function info {
+info() {
+    local dt
     dt=$(date)
+    local locip
     locip=$(sudo lshw | grep ip= | cut -d "=" -f 7 | cut -d " " -f 1)
+    local pubip
     pubip=$(dig +short myip.opendns.com @resolver1.opendns.com)
+    local cpu
     cpu=$(sudo hwinfo | grep "model name" | head -1 | cut -d ":" -f 2)
+    local gpu
     gpu=$(sudo hwinfo --gfxcard | grep Model | cut -d ":" -f 2)
+    local ram
     ram=$(sudo hwinfo --memory | grep Size | cut -d ":" -f 2)
+    local netcard
     netcard=$(sudo hwinfo --network | grep File | head -1 | cut -d ":" -f 2)
+    local sound
     sound=$(sudo hwinfo --sound | grep Model | cut -d ":" -f 2)
+    local disk
     disk=$(sudo hwinfo --disk | grep Model | cut -d ":" -f 2)
-    clear
     echo "--------------------------------"
     echo "PORTUNUS LOGFILE"
     echo "--------------------------------"
@@ -57,4 +78,8 @@ function info {
     echo "Public IP:                $pubip"
 }
 
-prep && deliver && info > $portunus/portunus.log
+main() {
+    prep && deliver && info >$PORTUNUS/portunus.log
+}
+
+main
